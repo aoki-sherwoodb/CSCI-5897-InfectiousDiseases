d8652f79e2ae3ab5b6687c0c64ffcff52b61b370
Ben Aoki-Sherwood <aokisherwoodb@gmail.com>
2025-10-13 22:44:46 -0600

    start hw2, add tex compiler files to gitignore

--- sir_models.py
+++ sir_models.py
@@ -19,3 +19,18 @@ def sir(state, beta, gamma):
     x_dot[1] = (beta * state[0] * state[1]) / state.sum() - gamma * state[1]
     x_dot[2] = gamma * state[1]
     return x_dot
+
+def sis(state, beta, gamma):
+    if state.shape[0] != 2:
+        raise ValueError("State be 2D representing S and I populations.")
+    
+    x_dot = np.zeros(2)
+    x_dot[0] = -(beta * state[0] * state[1]) + gamma * state[1]
+    x_dot[1] = (beta * state[0] * state[1]) - gamma * state[1]
+    return x_dot
+
+def closed_form_sis_i(t, beta, gamma, i_0):
+    R0 = beta / gamma
+    numerator = 1 - 1 / R0
+    denominator = 1 + ((1 - 1 / R0 - i_0) / i_0) * np.exp(-(beta - gamma) * t)
+    return numerator / denominator
